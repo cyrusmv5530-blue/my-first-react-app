2667062d92ef700edb2a320feeaa6c6cc8dffcf4
Cyrus <cyrusmv5530@gmail.com>
2026-05-08 09:02:49 -0700

    main map fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'flowbite';
-import { HashRouter } from "react-router-dom";
+import 'flowbite'
+import { HashRouter } from "react-router-dom"
 
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+import L from "leaflet"
+import "leaflet/dist/leaflet.css"
 
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png"
+import markerIcon from "leaflet/dist/images/marker-icon.png"
+import markerShadow from "leaflet/dist/images/marker-shadow.png"
 
-delete L.Icon.Default.prototype._getIconUrl;
+delete L.Icon.Default.prototype._getIconUrl
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
-});
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
